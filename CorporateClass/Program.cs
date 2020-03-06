328497a7e76192ab90f8dfa76da4f7c1adf8377f
Maria <mariabrock414@gmail.com>
2020-03-05 20:55:40 -0600

    Add project files.

---
+++ CorporateClass/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace CorporateClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var userEntry = Console.ReadLine();
+
+            var Employees = new string[] { "Amy", "Joel", "Phillip", "Sasha", "Brett", "Tilde" };
+
+        public class Company
+        {
+            // Some properties
+            public string Name { get; }
+            public DateTime CreatedOn { get; }
+
+            // Create a private property for holding an array of current employees
+
+
+
+            // Create a method that allows a caller to remove an employee
+
+
+
+            // Create a method that allows a caller to retrieve the list of employees
+
+
+
+            /*
+                Create a constructor method that accepts two arguments:
+                    1. The name of the company
+                    2. The date it was created
+
+                The constructor will set the value of the public properties
+            */
+        }
+
+        // Create a method that allows a caller to add an employee
+        class Employee
+        {
+
+
+            public string occupation { get; set; }
+            public string firstName { get; set; }
+            public string lastName { get; set; }
+
+            // Create an Employee
+            //Employee steve = new Employee();
+            //userEntry.firstName = "Steve";
+            //userEntry.lastName = "Brownlee";
+            //userEntry.occupation = "Developer";
+        }
+    }
+    }
+}
